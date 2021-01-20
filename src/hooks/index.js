eba4410d681361c1928b073c0bdc190a95d431bd
Myles Jeffery <mylesjeffery96@gmail.com>
2021-01-20 10:09:51 -0800

    added slider functionality

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,5 @@
-import useAuthListener from "./use-auth-listener"
+import useAuthListener from './use-auth-listener'
 
 export { default as useAuthListener } from './use-auth-listener'
 export { default as useContent } from './use-content'
+export { default as useWindowSize } from './windowresize'
